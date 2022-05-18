4579d606cd133d421941404daa36e5aea1e8f112
vivimcv <tenshi.nyaa@gmail.com>
2022-05-18 10:22:00 -0500

    Initial Commit

---
+++ Practica1-mod7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica1-mod7
+//
+//  Created by Viviana Marcela Cruz Vega on 18/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
